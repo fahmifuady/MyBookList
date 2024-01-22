0c0fd14678c4296478ba30387f4af71289a99582
Muhammad Husni Fahmi Fuady <husni.fuad22@gmail.com>
2024-01-22 15:05:39 +0700

    pages: add content placeholder

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,8 +2,13 @@
 	import { ImagePlaceholder, Skeleton, TextPlaceholder } from 'flowbite-svelte';
 </script>
 
-<div style="height:200vh;" class="overflow-scroll pb-16 px-2 sm:px-4 py-2.5">
-	<Skeleton class="mb-8 mt-16" />
-	<ImagePlaceholder class="my-8" />
-	<TextPlaceholder class="my-8" />
+<div class="h-20 w-full"></div>
+
+<!-- content here -->
+<div class="mx-auto w-4/5">
+	<div style="height:200vh;" class="overflow-auto px-2 py-2.5 pb-16 sm:px-4">
+		<Skeleton class="mb-8 mt-16" />
+		<ImagePlaceholder class="my-8" />
+		<TextPlaceholder class="my-8" />
+	</div>
 </div>
